41f9912f2ef39bd10dbf0a99378a423f2999dc88
BrianAwotwi <bawotwi1@gmail.com>
2025-03-11 12:07:00 -0400

    intergrated user profile on backend; updated giftRoutes and db js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -5,24 +5,26 @@ const MongoClient = require("mongodb").MongoClient;
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
-let dbInstance = null;
 const dbName = "giftdb";
+
+let dbInstance = null;
 
 async function connectToDatabase() {
   if (dbInstance) {
     return dbInstance;
   }
 
-  const client = new MongoClient(url);
+  try {
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log("Connected to MongoDB");
 
-  // Task 1: Connect to MongoDB
-  await client.connect();
-
-  // Task 2: Connect to database giftDB and store in variable dbInstance
-  let dbInstance = client.db(dbName);
-
-  // Task 3: Return database instance
-  return dbInstance;
+    dbInstance = client.db(dbName); // Assign to global dbInstance
+    return dbInstance;
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+    throw error;
+  }
 }
 
 module.exports = connectToDatabase;
